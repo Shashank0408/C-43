6e1324f5bc69a74e99884d1869e90b4e980e1f32
Spidy0408 <78598185+Shashank0408@users.noreply.github.com>
2021-10-23 12:36:46 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "INDIA";
+const accessCode2 = "WEST INDIES";
+const accessCode3 = "VIRAT KOHLI";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("D I N A I", 100,50)
+    fill("lightblue")
+    text("Hint: This team won first T20 World Cup", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("E T S W  I S N I E D", 700,150)
+    fill("lightblue")
+    text("Hint: This team won most ICC T20 trophies", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("R T V A I  H K I O L", 100,250)
+    fill("lightblue")
+    text("Hint: This player is the captain of team India in ICC T20 2021", 100,270)
+
+}
